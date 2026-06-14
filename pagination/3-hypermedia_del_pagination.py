9d8d6eb76fbd8310abf5f6ca0f61c3f7c76a41d0
quliyevadinara <quliyevadinara14@gmail.com>
2026-06-14 23:39:28 +0400

    fix

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -34,8 +34,7 @@ class Server:
             self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                         page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a dictionary with pagination metadata resilient to
         deletions.
         """
