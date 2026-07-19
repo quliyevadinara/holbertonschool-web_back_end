b33f5ac14a0f9a61757783bf1a05714c10a8817b
quliyevadinara <quliyevadinara14@gmail.com>
2026-07-19 21:03:44 +0400

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -10,7 +10,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       if (result.status === "fulfilled") {
         return { status: result.status, value: result.value };
       }
-      return { status: result.status, value: result.reason };
+      return { status: result.status, value: result.reason.toString() };
     });
   });
 }
